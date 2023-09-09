caf9f3d2c0df52fbc0679a84be166487098d1d23
UNKNOWNGHOST <unknownghost@macs-MacBook-Air.local>
2023-09-09 18:39:34 +0100

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const day = document.querySelector('[data-testid="currentDayOfTheWeek"]');
 const time = document.querySelector('[data-testid="currentUTCTime"]');
-const timeNow = `${new Date().getUTCHours()}:${new Date().getUTCMinutes()}:${new Date().getSeconds()}`;
+const timeNow = `${new Date().getUTCMilliseconds()}`;
 time.innerHTML = timeNow;
 day.innerHTML = new Date().toLocaleString("en-US", { weekday: "long" });
