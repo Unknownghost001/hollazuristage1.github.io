f8f402191a6d9b293358f58378ceb5cf0261d613
UNKNOWNGHOST <unknownghost@macs-MacBook-Air.local>
2023-09-10 09:31:54 +0100

    update

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-const day = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+/*const day = document.querySelector('[data-testid="currentDayOfTheWeek"]');
 const time = document.querySelector('[data-testid="currentUTCTime"]');
 const timeNow = `${new Date().getUTCMilliseconds()}`;
 time.innerHTML = timeNow;
 day.innerHTML = new Date().toLocaleString("en-US", { weekday: "long" });
+*/
+
+// alert(7)
+// Function to update the current UTC time
+function updateUTCTime() {
+    const currentDate = new Date();
+    const currentUTCTime = currentDate.getTime();
+    document.querySelector('[data-testid="currentUTCTime"]').textContent = currentUTCTime;
+}
+
+// Function to update the current day of the week
+function updateDayOfWeek() {
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currentDate = new Date();
+    const currentDayIndex = currentDate.getDay();
+    const currentDayOfTheWeek = daysOfWeek[currentDayIndex];
+    document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = currentDayOfTheWeek;
+}
+
+// Call the update functions initially
+updateUTCTime();
+updateDayOfWeek();
+
+// Set intervals to update the time and day of the week every second
+setInterval(updateUTCTime, 1000);
+setInterval(updateDayOfWeek, 1000);
