90a00736abf272bd58f16a3a455a3a2fd2475564
UNKNOWNGHOST <unknownghost@macs-MacBook-Air.local>
2023-09-10 09:36:57 +0100

    update

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-/*const day = document.querySelector('[data-testid="currentDayOfTheWeek"]');
-const time = document.querySelector('[data-testid="currentUTCTime"]');
-const timeNow = `${new Date().getUTCMilliseconds()}`;
-time.innerHTML = timeNow;
-day.innerHTML = new Date().toLocaleString("en-US", { weekday: "long" });
-*/
-
 // alert(7)
 // Function to update the current UTC time
 function updateUTCTime() {
